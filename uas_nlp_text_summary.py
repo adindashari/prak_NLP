6078390767119e2f7519c7d7f659377f7ce32157
Adinda <38552747+adindashari@users.noreply.github.com>
2021-06-13 21:14:34 +0700

    HTTP request NLP txt summary

--- uas_nlp_text_summary.py
+++ uas_nlp_text_summary.py
@@ -1,27 +1,50 @@
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-import bahasa
-news_txt = "Data pribadi dan informasi penting merupakan sasaran empuk bagi para hacker. Pasalnya, hacker akan memanfaatkan celah keamanan untuk mencuri data dan memanfaatkannya untuk hal-hal yang merugikan Anda. Tak heran jika keamanan data menjadi perhatian khusus bagi para pelaku bisnis dan usaha, sehingga mereka bersedia untuk melakukan berbagai hal demi mengamankannya. Salah satunya saja seperti IBM Storage Server Indonesia, yang bisa membantu Anda untuk meningkatkan sekuritas data penting dari tangan-tangan tak bertanggung jawab." \
-           "Nah, untuk Anda yang ingin mengamankan data digital dari serangan hacker, beberapa hal ini bisa dilakukan:" \
+import re # modul regular expression
+import requests # untuk library mengirim permintaan HTTP
+from nltk.corpus import stopwords #untuk mengahapus kata umum
+from nltk.tokenize import word_tokenize, sent_tokenize #penggunaan case folding
+from bs4 import BeautifulSoup # Library yang digunakan untuk melakukan ekstraksi file dengan format XML atau HTML
 
 
-stopwords = set(stopwords.words(bahasa))
-words = word_tokenize(news_txt)
+# 1
+headers = {
+    'User-Agent': 'Chrome/39.0.2171.95 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+req = requests.get(
+    'https://www.liputan6.com/tekno/read/4548651/serangan-ransomware-di-rumah-sakit-antara-hidup-dan-mati', headers=headers)
+soup = BeautifulSoup(req.content, "html.parser")
 
+# 2
+par_soup_title = soup.find('h1').text
+
+# 3
+baris = 0
+teks = ""
+for par_soup_body in soup.find_all('p'):
+    if baris > 0:
+        teks += par_soup_body.get_text()
+    baris += 1
+stopwords = set(stopwords.words("indonesian"))
+
+# 4
+words = word_tokenize(teks)
+
+# 5
 freqTable = dict()
-
 for word in words:
     word = word.lower()
-    if word in stopWords:
+    if word in stopwords:
         continue
-        if word in freqTable:
-            freqTable[word] += 1
-        else:
-            freqTable[word] = 1
+    if word in freqTable:
+        freqTable[word] += 1
+    else:
+        freqTable[word] = 1
 
 
-sentences = sent_tokenize(news_txt)
-sentenceValue = dict()
+# 6
+sentences = sent_tokenize(teks)
+sentenceValue = dict() # membuat variabel kamus kosongan
+
+# 7
 for sentence in sentences:
     for word, freq in freqTable.items():
         if word in sentence.lower():
@@ -30,17 +53,24 @@ for sentence in sentences:
             else:
                 sentenceValue[sentence] = freq
 
+# 8
 sumValues = 0
 for sentence in sentenceValue:
     sumValues += sentenceValue[sentence]
 
-
+# 9
 average = int(sumValues / len(sentenceValue))
 
+# 10
 summary = ''
 for sentence in sentences:
-    if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
-        summary += "" + sentence
+    if(sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
+        summary += " " + sentence
 
+# 11
+summary = re.sub('[^a-zA-Z0-9 \n\.\,]', '', summary)
 
+# 12
+print(par_soup_title)
+print("\n")
 print(summary)
